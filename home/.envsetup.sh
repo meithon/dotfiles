090bf6cbe14fbb757608f5475906b24fed824cf6
meithon <rneithon@gmail.com>
2024-10-05 19:02:32 +0900

    Up

--- home/.envsetup.sh
+++ home/.envsetup.sh
@@ -1,13 +1,19 @@
 export PATH="$HOME/.cargo/bin:$PATH"
 export PATH="$HOME/.local/share/bob/nvim-bin:$PATH"
 
+declare ASDF_DIR
+
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   # in linux
-  . "$HOME/.asdf/asdf.sh"
+  ASDF_DIR=$HOME/.asdf
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   # in mac
+  ASDF_DIR=/opt/homebrew/opt/asdf/libexec/
   export PATH=/opt/homebrew/bin:$PATH
 fi
+
+. $ASDF_DIR/asdf.sh
+
 export PATH=$(asdf where rust)/bin:$PATH
 export PATH=$(asdf where golang)/packages/bin:$PATH
 export JAVA_HOME=$(asdf where java)
