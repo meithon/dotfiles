04d1194700153d94337527d655d8e8277e5c929e
我 CRA Mei Wagatsuma <mei.wagatsuma@craftsman-software.com>
2024-11-15 21:31:55 +0900

    better zshrc

--- shell/alias.sh
+++ shell/alias.sh
@@ -6,7 +6,6 @@ alias vv="~/dotfiles/nvim-switcher/switcher.sh"
 alias lv="NVIM_APPNAME=LazyVim nvim"
 alias av="NVIM_APPNAME=AstroVim nvim"
 
-alias ksec=fzf-print-k8s-secret
 alias ll="lsd -l"
 alias la="lsd -l -a"
 alias ..="cd .."
@@ -32,7 +31,6 @@ alias grb="git rebase"
 alias grs="git reset"
 alias gitr="git tree"
 
-alias git-pretty-log="git log --graph --pretty=format:'%Cred%h%Creset %Cgreen(%ad) -%C(yellow)%d%Creset %s %C(bold blue)<%an>%Creset' --commit --date=format:'%Y-%m-%d %H:%M'"
 alias remain="git switch main && git pull && git switch - && git rebase main"
 
 ## mise
