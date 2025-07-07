a908c4ca9c249e313beaa4a35ede10c92a8923b5
MeiWagatsuma <wagamei256@gmail.com>
2025-07-07 15:58:18 +0900

    remove unused files

--- zsh/kubecli/functions.zsh
+++
@@ -1,9 +0,0 @@
-function delete-crd() {
-	local crd_list
-	crd_list=$(k get crd | cut -d " " -f 1 | tail -n +2 )
-	local selected
-	selected=$(echo "$crd_list" | fzf -m)
-	
-	echo $selected
-	# k delete crd $SELECTED -o yaml
-}
